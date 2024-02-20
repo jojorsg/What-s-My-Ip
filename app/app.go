79fa80f2c5243cd11cf99335c167fb5ed7e475e0
jojorsg <josueroberto099@gmail.com>
2024-02-20 15:51:22 -0300

    Criando a funcionalidade de busca por IP

--- app/app.go
+++ app/app.go
@@ -1,6 +1,12 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // Gerar vai retornar a aplicação de linha de comando pronta para ser executada
 func Gerar() *cli.App {
@@ -8,5 +14,31 @@ func Gerar() *cli.App {
 	app.Name = "What's My IP"
 	app.Usage = "Busca IP e servidor host"
 
+	app.Commands = []cli.Command {
+		{
+			Name: "ip",
+			Usage: "Busca IP de endereço web",
+			Flags: []cli.Flag {
+				cli.StringFlag {
+					Name:"host",
+					Value: "google.com",
+				},
+			},
+			Action: buscarIp,
+		},
+	}
 	return app
+}
+
+func buscarIp(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
